d7bb1a338845195b565d28a2af75b76755b96a6a
Erick Bernardo <erickvictor@gmail.com>
2021-04-09 18:58:19 -0400

    Add NextSEO and NProgress

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.erickbernardo.com.br/',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@erickvictor',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
